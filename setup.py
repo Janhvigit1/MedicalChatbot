ba07ebcebc7c79412797899e3ab4364c19a62c4c
Janhvigit1 <factzit444@gmail.com>
2026-02-28 16:14:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="mental-chatbot",
+    version="0.1.0",
+    author="Janhvi",
+    author_email="janhvilogin31@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
